49ba7a328094afe7405cdf3002be306ff4e9cc51
Davide Angelocola <davide.angelocola@gmail.com>
2011-11-22 00:02:33 +0100

    ref: moved away pratt's algorithm

--- src/pysem/test/test_tokenizer.py
+++ src/pysem/test/test_tokenizer.py
@@ -1,16 +1,11 @@
 from unittest import TestCase
-from tokenizer import tokenize, parse, evaluate
+from tokenizer import tokenize
 
 class TokenizerTest(TestCase):
     
-    # def test_number(self):
-    #     self.assertEqual(["0", "END"], list(tokenize("0")))
+    def test_number(self):
+        self.assertEqual([("NUMBER", "0"), ("END", None)], list(tokenize("0")))
+        
+    def test_plus(self):
+        self.assertEqual([("+", None), ("END", None)], list(tokenize("+")))
 
-    # def test_plus(self):
-    #     self.assertEqual(["+", "END"], list(tokenize("+")))
-
-    def test_eval(self):
-        self.assertEqual(3, evaluate(parse("1+2")))
-
-    def test_eval2(self):
-        self.assertEqual(6, evaluate(parse("1+2+3")))
--- src/pysem/tokenizer.py
+++ src/pysem/tokenizer.py
@@ -5,67 +5,9 @@ TOKENS = compile("\s*(?:(\d+)|(.))")
 def tokenize(line):
     for number, operator in TOKENS.findall(line):
         if number:
-            yield literal_token(number)
+            yield ("NUMBER", number)
         elif operator == '+':
-            yield add_token()
+            yield ("+", None)
         else:
             raise SyntaxError("unknown operator")
-    yield end_token()
-       
-class literal_token:
-    lbp = 1
-    def __init__(self, value):
-        self.value = value
-    def nud(self):  
-        return self
-    def __str__(self): 
-        return "Literal({})".format(self.value)
-
-class add_token:
-    lbp = 10
-    def led(self, tokens, left):
-        self.right = expression(tokens, 10)
-        self.left = left
-        return self
-    def __str__(self):
-        return "Plus({},{})".format(self.left, self.right)
-
-class end_token:
-    lbp = 0
-    def led(self, tokens, left):
-        self.left = left
-        return self
-    def __str__(self):
-        return "End({})".format(self.left)
- 
-def expression(tokens, rbp = 0):
-    """Pratt's algorithm
-
-    nud is null denotation for tokens that appears to the beginning of a rule
-    led is left denotation when it appears in the construct
-    lbp is the binding power (precedence)
-    """
-    token = next(tokens)
-    left = token.nud()
-    while rbp < token.lbp:
-        token = next(tokens)
-        left = token.led(tokens, left)
-    return left
-
-def parse(line):
-    return expression(tokenize(line))
-
-def evaluate(node):
-    if hasattr(node, 'left'):
-        left = evaluate(node.left) 
-
-        if hasattr(node, 'right'):
-            left += evaluate(node.right)
-
-        return left 
-
-    if hasattr(node, 'value'):
-        return int(node.value)
-    raise Error("aoh")
-
-            
+    yield ("END", None)
