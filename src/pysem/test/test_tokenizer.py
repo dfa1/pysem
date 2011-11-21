bc353f2cb2e1c3aca2d6f18b30eada3b70aac225
Davide Angelocola <davide.angelocola@gmail.com>
2011-11-22 00:30:26 +0100

    enh: support for operators, strings and identifiers

--- src/pysem/test/test_tokenizer.py
+++ src/pysem/test/test_tokenizer.py
@@ -3,9 +3,29 @@ from tokenizer import tokenize
 
 class TokenizerTest(TestCase):
     
+    def test_empty(self):
+        self.assertEqual(("END", None), next(tokenize("")))
+
     def test_number(self):
-        self.assertEqual([("NUMBER", "0"), ("END", None)], list(tokenize("0")))
+        self.assertEqual(("NUMBER", "0"), next(tokenize("0")))
         
     def test_plus(self):
-        self.assertEqual([("+", None), ("END", None)], list(tokenize("+")))
+        self.assertEqual(("+", None), next(tokenize("+")))
+
+    def test_minus(self):
+        self.assertEqual(("-", None), next(tokenize("-")))
+
+    def test_name(self):
+        self.assertEqual(("NAME", "foo"), next(tokenize("foo")))
+
+    def test_string(self):
+        self.assertEqual(("STRING", "foo 123!"), next(tokenize("\"foo 123!\"")))
+
+    def test_end_is_the_last_token(self):
+        for last in tokenize("foo"): pass
+        self.assertEqual(("END", None), last)
+
+    def test_unknown_token_leads_to_syntax_error(self):
+        with self.assertRaises(SyntaxError):
+            next(tokenize("%%"))
 
--- src/pysem/tokenizer.py
+++ src/pysem/tokenizer.py
@@ -1,13 +1,19 @@
 from re import compile
 
-TOKENS = compile("\s*(?:(\d+)|(.))")
+TOKENS = compile("\s*(?:(\d+)|(\w+)|\"(.*)\"|(.+))")
 
 def tokenize(line):
-    for number, operator in TOKENS.findall(line):
+    for number, name, string, operator in TOKENS.findall(line):
         if number:
             yield ("NUMBER", number)
+        elif name:
+            yield ("NAME", name)
+        elif string:
+            yield ("STRING", string)
         elif operator == '+':
             yield ("+", None)
+        elif operator == '-':
+            yield ("-", None)
         else:
-            raise SyntaxError("unknown operator")
+            raise SyntaxError("unknown token '{}'".format(operator))
     yield ("END", None)
